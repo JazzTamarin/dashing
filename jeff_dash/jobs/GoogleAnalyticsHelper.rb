621eb0a9cf21ee842328e7f97ac49597489829d2
Jeff Bayntun <j_bayntun@hotmail.com>
2015-04-27 15:27:43 -0700

    made googleanalyticshelper a singleton

--- jeff_dash/jobs/GoogleAnalyticsHelper.rb
+++ jeff_dash/jobs/GoogleAnalyticsHelper.rb
@@ -5,9 +5,15 @@ class GoogleAnalyticsHelper
 	def initialize
 		@today = Time.now
 		@current_start_day = @today - MONTH
-		@previous_start_day = @current_start_day - MONTH
+		@previous_start_day = @current_start_day - MONTH		
 	end
 attr_reader :today, :current_start_day, :previous_start_day, :current_web_visits, :previous_web_visits
+
+	@@instance = GoogleAnalyticsHelper.new
+
+	def self.instance
+		return @@instance
+	end
 
 	def update_times
 		@today = Time.now
@@ -18,5 +24,7 @@ attr_reader :today, :current_start_day, :previous_start_day, :current_web_visits
 	def get_web_visits(start, finish)
 		5000 + rand(500)
 	end
+
+	 private_class_method :new
 
 end
